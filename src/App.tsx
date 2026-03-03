59bed22c30a0f973c489e10a9059ebabcdb6c058
arnavsharma1811 <arnavsharmaone8@gmail.com>
2026-03-03 17:26:14 +0530

    set up the Dock

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import Dock from "./components/Dock";
 import Navbar from "./components/Navbar";
 import Welcome from "./components/Welcome";
 const App = () => {
   return ( <main> 
     <Navbar />
     <Welcome />
+    <Dock />
   </main>)
 };
 
