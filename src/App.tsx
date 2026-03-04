65501c202e7935687e097e437eaf205641340cd2
arnavsharma1811 <arnavsharmaone8@gmail.com>
2026-03-05 00:02:10 +0530

    set up the high order component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import Dock from "./components/Dock";
 import Navbar from "./components/Navbar";
 import Welcome from "./components/Welcome";
+import gsap from "gsap";
+import { Draggable } from "gsap/Draggable";
+gsap.registerPlugin(Draggable);
+
 const App = () => {
   return ( <main> 
     <Navbar />
     <Welcome />
     <Dock />
-  </main>)
+  </main>
+  )
 };
 
 export default App
---
+++ src/hoc/WindowWrapper.tsx
@@ -0,0 +1,25 @@
+import useWindowStore from "../store/window";
+import { useRef } from "react";
+
+const WindowWrapper = (Component, WindowKey) => {
+    const Wrapped = (props) => {
+        const {focusWindow, windows} = useWindowStore();
+        const {isOpen, zIndex} = windows[windowKey];
+        const ref = useRef(null);
+    
+    return (
+        <section id = {windowKey} ref = {ref} style = {{zIndex}}
+        className="absolute"
+        >
+        <Component {... props} />
+
+        </section>
+    )
+   
+}
+ Wrapped.displayName = `WindowWrapper(${Component.displayName || Component.name || "Component"}`
+    
+ return Wrapped
+}
+
+export default WindowWrapper;
